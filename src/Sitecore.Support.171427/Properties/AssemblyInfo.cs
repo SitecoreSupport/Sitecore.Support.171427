2b6e1178bacb08a76bf850b33458cfc50275a2c5
DK\dmsa <dmsa@sitecore.net>
2017-08-28 11:01:50 +0300

    Fix for the issue #171427 --- Special encoded symbols in rich text links get decoded and duplicated

---
+++ src/Sitecore.Support.171427/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.171427")]
+[assembly: AssemblyProduct("Sitecore.Support.171427")]
+[assembly: ComVisible(false)]
